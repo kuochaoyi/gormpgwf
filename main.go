d320002053a833365f0ab116a0e15044bbc21765
ChaoYi Kuo <master@wisdomfish.org>
2020-12-23 14:35:09 +0800

    Save json to PG Jsonb is done.

--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import (
 	"goland_gorm/database"
 	_ "goland_gorm/database"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jinzhu/gorm/dialects/postgres"
 )
 
 func main() {
 	// database.Open()
 	// database.DBClient.Insert()
-	database.DBClient.DB.AutoMigrate(&database.BaseModelSoftDelete{})
-	database.DBClient.DB.Create(&database.BaseModelSoftDelete{})
+	db := database.DBClient.DB
+	json := `{"age":  27, "name": "Yan"}`
+
+	db.AutoMigrate(&database.BaseModelSoftDelete{})
+	db.Create(&database.BaseModelSoftDelete{
+		BaseModel: database.BaseModel{
+			ID:        uuid.UUID{},
+			CreatedAt: time.Time{},
+			UpdatedAt: nil,
+			BaseModelJsonb: database.BaseModelJsonb{
+				JsonStore: postgres.Jsonb{
+					RawMessage: []byte(json),
+				},
+			},
+		},
+		DeletedAt: nil,
+	})
+
 }
