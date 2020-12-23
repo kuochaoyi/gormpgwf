fdfcb88b6dfd70e7213833f9e00fe3f1a85a89a8
ChaoYi Kuo <master@wisdomfish.org>
2020-12-23 16:00:12 +0800

    done.

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"goland_gorm/database"
 	_ "goland_gorm/database"
+	"log"
+	"os"
 	"time"
 
 	"github.com/google/uuid"
@@ -13,7 +16,19 @@ func main() {
 	// database.Open()
 	// database.DBClient.Insert()
 	db := database.DBClient.DB
-	json := `{"age":  27, "name": "Yan"}`
+	// json := `{"age":  27, "name": "Yan"}`
+	json1 := `{
+	"name": "Pasta",
+		"ingredients": ["Flour", "Eggs", "Salt", "Water"],
+"organic": true,
+"dimensions": {
+"weight": 500.00
+}
+}`
+	json2 := `{"kuo": "chaoYi"}`
+
+	jb := new(database.BaseModelSoftDelete)
+	jb.JsonStore.RawMessage = []byte(json2)
 
 	db.AutoMigrate(&database.BaseModelSoftDelete{})
 	db.Create(&database.BaseModelSoftDelete{
@@ -23,11 +38,36 @@ func main() {
 			UpdatedAt: nil,
 			BaseModelJsonb: database.BaseModelJsonb{
 				JsonStore: postgres.Jsonb{
-					RawMessage: []byte(json),
+					RawMessage: []byte(json1),
 				},
 			},
 		},
 		DeletedAt: nil,
 	})
 
+	db.Create(&jb)
+	var result database.BaseModelSoftDelete
+	db.First(&result)
+	log.Printf("Println this a objcet: %s", &result)
+
+	a := &result.JsonStore
+	b, _ := json.Marshal(a)
+	log.Printf("json.Marshal(): %s", a) // []byte
+	os.Stdout.Write(b)                  // json
+
+	/*	layout := "2006-01-02T15:04:05.000Z"
+		str := &result.CreatedAt
+		t, err := time.Parse(layout, str)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(t)*/
+
+	/*	if result.JsonStore.RawMessage == []byte(json2) {
+			fmt.Println("SUCCESS: Selected JSON == inserted JSON")
+		} else {
+			fmt.Println("FAILED: Selected JSON != inserted JSON")
+			fmt.Println("Inserted: " + STATE)
+			fmt.Println("Selected: " + result.State)
+		}*/
 }
