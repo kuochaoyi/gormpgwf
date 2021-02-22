58ffbcda8390748c9e5deb43a386175c0b3ca157
Kuo, Chao-Yi <kuo.chaoyi@gmail.com>
2021-02-23 01:50:38 +0800

    Modifi reporting - Purchase by dalis.

--- sql/psi/views.sql
+++ sql/psi/views.sql
@@ -1,7 +1,9 @@
  -- 累積進貨量(all)
  SELECT psi_p_logs.order_id,
     psi_p_logs.product_id,
+    psi_p_stock.product_no,
     psi_p_logs.quantity
-   FROM psi_p_logs
-  WHERE psi_p_logs.deleted_at IS NULL
+   FROM psi_p_logs,
+    psi_p_stock
+  WHERE psi_p_logs.product_id = psi_p_stock.uuid_id AND psi_p_logs.deleted_at IS NULL
   ORDER BY psi_p_logs.order_id DESC;
